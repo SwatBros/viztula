f76d6c2e2c3d333c88d6d3ce59d935a348484397
Alessio Disarò <alessiobesnate@gmail.com>
2024-06-27 11:20:45 +0200

    feat: added basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
+use sqlx::PgPool;
 
 mod chart;
 mod utils;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
     
     let pool = PgPool::connect("postgresql://postgres:password@localhost:5432/postgres")
         .await
@@ -16,7 +19,7 @@ async fn main() -> std::io::Result<()> {
             .service(chart::service::chart)
             .service(chart::service::data)
     })
-        .bind(("127.0.0.1", 8999))?
-        .run()
-        .await
+    .bind(("127.0.0.1", 8999))?
+    .run()
+    .await
 }
